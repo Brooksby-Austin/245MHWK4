0fa71fa8326d9c4f06ecc080edc1d415fab1c46e
Brooksby-Austin <brooksby.austin@gmail.com>
2023-10-23 19:55:12 -0700

    Waiting for lecture to cover material

--- assignment_4.R
+++ assignment_4.R
@@ -4,3 +4,12 @@ polls_adjusted <- read_csv("covid_approval_polls_adjusted.csv")
 
 #1
 #a
+polls_adjusted <- polls_adjusted |>
+  group_by(subject) |>
+  mutate(average_approval = mean(approve_adjusted, na.rm = TRUE))
+
+#b
+hist(polls_adjusted$approve_adjusted, xlab = "Approval", ylab= "Count")
+theme_minimal()
+
+#c
